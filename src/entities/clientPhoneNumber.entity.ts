ba5f39a76342f0c271ee39defb4cfa763600ff7d
Fernando Scramignon <scramignonnarde@gmail.com>
2022-11-29 19:26:11 -0300

    feat: creates client phone number entity

---
+++ src/entities/clientPhoneNumber.entity.ts
@@ -0,0 +1,10 @@
+import { Entity, PrimaryColumn, Column } from "typeorm";
+
+@Entity("ClientPhoneNumber")
+export class ClientPhoneNumber {
+    @PrimaryColumn({ unique: true })
+    phoneNumber: number
+
+    @Column({ nullable: false, default: false })
+    isMain: boolean
+}
