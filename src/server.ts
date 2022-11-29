564b70c315a61ad97bdccf4b2bfd8d4fb7c4d52b
Fernando Scramignon <scramignonnarde@gmail.com>
2022-11-29 17:19:14 -0300

    feat: configures app data source

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import "dotenv/config"
-import { app } from "./app";
 
-function startServer() {
-    const port = process.env.PORT;
+import { app } from "./app";
+import { appDataSource } from "./data-source";
+
+async function startServer() {
+    await appDataSource.initialize()
+        .then(() => console.log("Data source has been initialized"))
+        .catch(() => console.error("Error during Data Source initialization"))
+
+    const port = process.env.API_PORT;
     app.listen(port, () => console.log(`The server is running on port ${port}`));
 };
 
