f0176ad1584684fbb6b397915c44068f57fdf5f8
Fernando Scramignon <scramignonnarde@gmail.com>
2022-11-29 16:34:09 -0300

    feat: adds global errors test routes

--- src/routes/test.routes.ts
+++ src/routes/test.routes.ts
@@ -1,11 +1,20 @@
 import { Express } from "express";
 import { Request, Response } from "express";
 
+import { AppError } from "../errors/appError";
 
+
+// This test global error system
 function testRoutes(app: Express) {
     app.get("/test", (req: Request, res: Response) => {
         return res.send({ "msg": "hello world!" });
     });
+    app.get("/error", (req: Request, res: Response) => {
+        throw new AppError(400, "Error is in fact working");
+    })
+    app.get("/uncaughtError", (req: Request, res: Response) => {
+        throw new Error
+    })
 };
 
 export { testRoutes };
