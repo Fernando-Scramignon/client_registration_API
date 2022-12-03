a00fe15b206bb45da66eacd045d4edfb3042d294
Fernando Scramignon <scramignonnarde@gmail.com>
2022-12-03 01:38:34 -0300

    feat: adds specific contact listing

--- src/routes/contact.routes.ts
+++ src/routes/contact.routes.ts
@@ -5,12 +5,15 @@ import { Express } from "express";
 import { createContactController } from "../controllers/contact/createContact.controller";
 import { deleteContactController } from "../controllers/contact/deleteContact.controller";
 import { listContactsController } from "../controllers/contact/listContacts.controller";
+import { listSpecificContactController } from "../controllers/contact/listSpecificContact.controller";
 
 // middlewares
 import { verifyAuthMiddleware } from "../middlewares/verifyAuth.middleware";
 
 export function contactRoutes(app: Express) {
     app.post("/contacts", verifyAuthMiddleware, createContactController);
-    app.delete("/contacts/:contactId", verifyAuthMiddleware, deleteContactController);
     app.get("/contacts", verifyAuthMiddleware, listContactsController);
+
+    app.delete("/contacts/:contactId", verifyAuthMiddleware, deleteContactController);
+    app.get("/contacts/:contactId", verifyAuthMiddleware, listSpecificContactController);
 }
