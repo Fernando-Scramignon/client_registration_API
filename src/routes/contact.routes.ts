eefe46d3af0a283c96326d2318e45bfacfd15968
Fernando Scramignon <scramignonnarde@gmail.com>
2022-12-03 01:06:23 -0300

    feat: adds contact deletion route

--- src/routes/contact.routes.ts
+++ src/routes/contact.routes.ts
@@ -3,10 +3,12 @@ import { Express } from "express";
 
 // controllers
 import { createContactController } from "../controllers/contact/createContact.controller";
+import { deleteContactController } from "../controllers/contact/deleteContact.controller";
 
 // middlewares
 import { verifyAuthMiddleware } from "../middlewares/verifyAuth.middleware";
 
 export function contactRoutes(app: Express) {
     app.post("/contacts", verifyAuthMiddleware, createContactController);
+    app.delete("/contacts/:contactId", verifyAuthMiddleware, deleteContactController);
 }
